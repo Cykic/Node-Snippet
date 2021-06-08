9ff66f89daeba280cfccc0ee6a5c7e71c406aa40
cykic <cyb3rcykic@gmail.com>
2021-06-08 13:05:58 -0400

    lodash

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  // LODASH
+  // const num = _.random(0, 20);
+  // console.log(num);
 
+  // const greet =_.once(()=>{
+  //   console.log("Hello");
+  // })
+
+greet()
+greet()
   let path = "./views/";
   switch (req.url) {
     case "/":
@@ -14,10 +23,10 @@ const server = http.createServer((req, res) => {
       path += "about.html";
       res.statusCode = 200;
       break;
-    case "/about-me":
+    case "/about-us":
       res.statusCode = 301;
-      res.setHeader('Location','/about')
-      res.end()
+      res.setHeader("Location", "/about");
+      res.end();
       break;
     default:
       path += "404.html";

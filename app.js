26a1fd3b2aeb1bcaa107306a862e04d84b1e3eb5
cykic <cyb3rcykic@gmail.com>
2021-06-11 07:24:46 -0400

    MongDB origin

--- app.js
+++ app.js
@@ -1,42 +1,88 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
+// Connect to mongoDB
+const dbURI =
+  "mongodb+srv://Cykic:Fortunatus@node-snippets.vcwve.mongodb.net/Node-Snippets?retryWrites=true&w=majority";
+
+// object to prevent deprecation warning
+const deprecationWarning = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(dbURI, deprecationWarning);
+
 // register view engine
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
 // Listen for request
+app.listen(1234);
 
-app.listen(9999);
+// makes static files available from our sever
+app.use(express.static("public"));
+
+// 3rd Party Middleware
+app.use(morgan("dev"));
+const blogs = [
+  {
+    title: "Yoshi find Eggs",
+    snippet:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, tempore nemo ratione eligendi magnam itaque totam sunt quam tenetur vel atque cupiditate fugit. Voluptatibus eveniet modi, cupiditate voluptas iusto deserunt.",
+  },
+  {
+    title: "Mario Finds stars",
+    snippet:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, tempore nemo ratione eligendi magnam itaque totam sunt quam tenetur vel atque cupiditate fugit. Voluptatibus eveniet modi, cupiditate voluptas iusto deserunt.",
+  },
+  {
+    title: "How to defeat bowser",
+    snippet:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, tempore nemo ratione eligendi magnam itaque totam sunt quam tenetur vel atque cupiditate fugit. Voluptatibus eveniet modi, cupiditate voluptas iusto deserunt.",
+  },
+];
+
+//
 
 // ROUTES
 
 // HOME
 app.get("/", (req, res) => {
-//   res.sendFile("./views/index.html", { root: __dirname });
-res.render('index')
-console.log("home");
+  res.render("index", { title: "Home", blogs });
 });
 
 // ABOUT
 app.get("/about", (req, res) => {
-//   res.sendFile("./views/about.html", { root: __dirname });
-    res.render('about')
-  console.log("about");
-
+  res.render("about", { title: "About" });
 });
 
-// REDIRECT
-app.get("/about", (req, res) => {
-  res.redirect("/about");
-  console.log("redirect");
-
+//
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create Blog" });
 });
-
 // 404
 app.use((_, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
-  console.log("404");
-
+  res.status(404).render("404", { title: "404" });
 });
+
+// SNIPPETS
+
+// REDIRECT
+// app.get("/about", (req, res) => {
+//   res.redirect("/about");
+//   console.log("redirect");
+//   res.sendFile("./views/about.html", { root: __dirname });
+// });
+
+// MIDDLEWARE FOR EVERY REQUEST
+// app.use((req, res, next) => {
+//   console.log("New request made:");
+//   console.log("Host:", req.hostname);
+//   console.log("Path:", req.path);
+//   console.log("Method:", req.method);
+//   next();
+// });
